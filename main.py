5cbad1f11d572a0145edf1fd87d2d9cfcc23c75f
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2023-05-24 08:57:30 +0000

    replit

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ PREFIX = '.'
 CONFIG_FILE = 'bot_config/config.json'
 
 with open(CONFIG_FILE, encoding='utf8') as data:
-    config = json.load(data)
+  config = json.load(data)
 
-bot = commands.Bot(command_prefix=PREFIX, case_insensitive=True, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=PREFIX,
+                   case_insensitive=True,
+                   intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(bot))
+  print('We have logged in as {0.user}'.format(bot))
 
-@bot.command(name = 'ping')
+
+@bot.command(name='ping')
 async def ping(ctx):
-    print('pong')
-    await ctx.send(f"Pong! :ping_pong: `{round(bot.latency * 1000, 2)} ms`")
+  print('pong')
+  await ctx.send(f"Pong! :ping_pong: `{round(bot.latency * 1000, 2)} ms`")
+
 
 token = os.environ['DISCORD_BOT_SECRET']
 
-bot.run(token])
-        
+bot.run(token)
