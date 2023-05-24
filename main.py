5f2b9384f1347e4521890df717e3abf9db68d2cf
siddeshshewde <siddesh.darkeyed95@gmail.com>
2023-05-24 04:39:01 -0400

    Added Ping Command

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import json
+
+PREFIX = '.'
+CONFIG_FILE = 'bot_config/config.json'
+
+with open(CONFIG_FILE, encoding='utf8') as data:
+    config = json.load(data)
+
+bot = commands.Bot(command_prefix=PREFIX, case_insensitive=True, intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(bot))
+
+@bot.command(name = 'ping')
+async def ping(ctx):
+    print('pong')
+    await ctx.send(f"Pong! :ping_pong: `{round(bot.latency * 1000, 2)} ms`")
+
+bot.run(config['TOKEN'])
+        
